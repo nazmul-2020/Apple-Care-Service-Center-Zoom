88c94b2ec835e29e652cab72c2d768a8e9bc472a
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-09-03 18:48:01 +0600

    added banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,41 +1,41 @@
 import React from 'react';
+import banner from '../../../assets/Images/banner.png'
 
 const Banner = () => {
     return (
         <div className='mt-4'>
-            <div className='container'>
-                <div className='text-center'>
-                    <span className='fw-bold'>PRODUCTS </span>
-                    <span className='mx-1'>|</span>
-                    <span className='fw-bold'>SERVICE </span>
-                    <span className='mx-1'>|</span>
-                    <span className='fw-bold'>SHOP</span>
-                </div >
+            <div className=''>
+                <div className='container'>
+                    <div className='text-center'>
+                        <span className='fw-bold'>PRODUCTS </span>
+                        <span className='mx-1'>|</span>
+                        <span className='fw-bold'>SERVICE </span>
+                        <span className='mx-1'>|</span>
+                        <span className='fw-bold'>SHOP</span>
+                    </div >
 
-                <div className='sticky-top pt-1 '>
-                    <input class="form-control mx-auto mt-3 rounded-pill w-50  " type="search" placeholder="Search Products" aria-label="Search"></input>
-                </div>
-
-                {/* <nav class="navbar sticky-top bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Sticky top</a>
+                    {/* ------------ Search bar --------------------*/}
+                    <div className='sticky-top pt-1 '>
+                        <input class="form-control mx-auto mt-3 rounded-pill w-50  " type="search" placeholder="Search Products" aria-label="Search"></input>
                     </div>
-                </nav> */}
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
-                <h1>ssss</h1>
+
+
+                </div>
+                
+                <div className='row m-0'>
+                    <div className='col-12 col-md-6 col-lg-6 d-flex justify-content-center align-items-center '>
+                        <div className=''>
+                            <h3>PROFESSIONAL</h3>
+                            <h1 className='mb-3'>Apple Service Center</h1>
+                            <button type="button" class="btn btn-outline-dark me-4">REPAIR NOW</button>
+                            <button type="button" class="btn btn-outline-dark">Buy Now</button>
+                        </div>
+                    </div>
+
+                    <div className='col-12 col-md-6 col-lg-6  bg-dager d-flex justify-content-center align-items-center'>
+                        <img className='w-100' src={banner} alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     );
