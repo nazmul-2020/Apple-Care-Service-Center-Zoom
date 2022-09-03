3c703753853146f291d5abeb59f531c1e33117be
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-09-03 19:16:45 +0600

    added about

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TSJR from '../../../assets/Images/TSJR 1.png'
+
+
+const About = () => {
+    return (
+        <div className='my-5'>
+            <div className='row container mx-auto'>
+                <div className='col-12 col-md-6 col-lg-6 d-flex justify-content-center align-items-center'>
+                    <div>
+                        <h3 className='w-75'>We Can iCare Apple Almost As Fast As You Can Break It</h3>
+                        <p>To solve any kind of problem-related to your Apple devices, we have all kinds of modern technologies, a highly developed lab, and skilled technicians.</p>
+                        <button type="button" class="btn btn-outline-dark rounded-pill">About Us</button>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-6'>
+                    <img className='w-100' src={TSJR} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
