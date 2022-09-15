89f3a8479cd0a50dc81921a0e1c6b1c43fab3e7f
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-09-15 17:47:19 +0600

    added animation

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -11,6 +11,11 @@ const NotFound = () => {
                     <p>The requested URL was not found on this server.</p>
                     <p>Additionally, a 404 Not Found error was encountered while trying to use an ErrorDocument to handle the request.</p>
                     <Link className='fs-5 text-info fw-bold' to='/'>Home</Link>
+                    <br />
+                    <span className='menu-link'>menu-link</span>
+
+
+
                 </div>
             </div>
         </div>
